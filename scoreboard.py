01795c7d1ee88b2ac1423ba4304059f244a74616
pabitraale <pabitraale25@yahoo.com>
2024-01-15 16:32:06 -0500

    added scoreboard class

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 280)
+        self.write(f"Score :  {self.score}", align="center", font=('Arial', 12, 'normal'))
+        self.hideturtle()
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.write(f"Score :  {self.score}", align="center", font=('Arial', 12, 'normal'))
+
+
+
+
+
+
+
