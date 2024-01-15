d63e7c8dcd9b66f0651160fa0d5ba2c94b0bc53c
pabitraale <pabitraale25@yahoo.com>
2024-01-15 16:38:36 -0500

    added function to scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -7,14 +7,14 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 280)
-        self.write(f"Score :  {self.score}", align="center", font=('Arial', 12, 'normal'))
+        self.goto(0, 275)
+        self.write(f"Score :  {self.score}", align="center", font=('Courier', 16, 'normal'))
         self.hideturtle()
 
     def increase_score(self):
         self.clear()
         self.score += 1
-        self.write(f"Score :  {self.score}", align="center", font=('Arial', 12, 'normal'))
+        self.write(f"Score :  {self.score}", align="center", font=('Courier', 16, 'normal'))
 
 
 
